bbc3c4bb69e320aa3506b2f4f4b69899f5226c20
NewPenguin <chongjorminggg@gmail.com>
2025-02-15 16:40:45 +0800

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Activit App',
+      title: 'Activity App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
